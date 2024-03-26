f11b8affe1180216a813c42bca5b11b96c4d06c0
ramadan22 <harisramadan22@gmail.com>
2024-03-27 01:32:34 +0700

    update

--- src/config/sequelize-config.js
+++ src/config/sequelize-config.js
@@ -12,6 +12,13 @@ module.exports = {
     dialect: 'mysql',
   },
   production: {
-    // Konfigurasi produksi jika diperlukan
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    // host: "localhost",
+    port: process.env.MYSQL_DOCKER_PORT,
+    // port: process.env.DB_PORT,
+    dialect: 'mysql',
   },
 };
