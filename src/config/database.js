067cf59216e5cf4dbebe7f23b77830502d1723d8
ramadan22 <harisramadan22@gmail.com>
2024-03-28 01:03:34 +0700

    update

--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,7 @@
 const mysql = require('mysql2');
 
 const dbPool = mysql.createPool({
-  host: "database-mysql",
+  host: "custom-mysql",
   user: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
